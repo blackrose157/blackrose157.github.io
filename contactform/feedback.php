3587857865b6397f5d90d252b7de8d652670732d
blackrose157 <61098758+blackrose157@users.noreply.github.com>
2020-06-10 08:22:19 +0200

    4

--- contactform/feedback.php
+++ contactform/feedback.php
@@ -1,11 +1,19 @@
 <?php
+
 $to = "gm@info-tk.com"; 
   $subject = "**Message From ALSAD ALAALY  website**";
   $message = "Name : " . $_POST['username'] . "\n";
   $message .= "Email : " . $_POST['email'] . "\n";
   $message .= "Phone : " . $_POST['phone'] . "\n";
-  $message .= "Website : " . $_POST['Website'] . "\n";
+  $message .= "Website : " . $_POST['website'] . "\n";
   $message .= "Message : " . "\r\n". $_POST['message'] ;
-mail($to, $subject, $message);
+
+
+#Send email
+mail($to, $subject, $message) or die("Error!");
+echo "<script>
+window.location.href='contact.html';
+alert('Your message successfully sent');
+</script>";
 
 ?>
